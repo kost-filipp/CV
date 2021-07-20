8d97c0c9c46facaee6c3547da2012871e92c9125
kost-filipp <33116770+kost-filipp@users.noreply.github.com>
2021-07-20 17:45:15 +0300

    Update script.js

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -3,10 +3,10 @@ let eng_btn = document.getElementById('eng-btn');
 
 rus_btn.addEventListener('click', () => {
     localStorage.setItem('choose_lang', 'rus');
-    window.location.replace('http://127.0.0.1:8080/');
+    window.location.replace('/');
 })
 
 eng_btn.addEventListener('click', () => {
     localStorage.setItem('choose_lang', 'eng');
-    window.location.replace('http://127.0.0.1:8080/EN');
+    window.location.replace('/EN');
 })
